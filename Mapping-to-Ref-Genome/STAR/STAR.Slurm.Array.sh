eb4afb642f05e8b28517743be0b3c9d32a1c2d87
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-02-24 23:15:02 +0000

    Add files via upload

---
+++ Mapping-to-Ref-Genome/STAR/STAR.Slurm.Array.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#SBATCH -A Research_Project-MRC164847 # research project to submit under.
+#SBATCH --export=ALL # export all environment variables to the batch job.
+#SBATCH -D . # set working directory to .
+#SBATCH -p mrcq # submit to the parallel test queue
+#SBATCH --time=5:00:00 # Maximum wall time for the job
+#SBATCH --nodes=1 # specify number of nodes.
+#SBATCH --ntasks-per-node=16 # specify number of processors.
+#SBATCH --mail-type=END # send email at job completion
+#SBATCH --mail-user=m.kouhsar@exeter.ac.uk # email address
+#SBATCH --array=0-15
+#SBATCH --job-name=STAR
+
+echo Job started on:
+date -u
+
+InDir=./RNASeq.fastq
+GenomeIndexDir=./STAR/genome_index
+OutDir=./STAR/Results
+
+module load STAR
+
+mkdir -p $OutDir
+mkdir -p ${OutDir}/temp
+
+samples=($(ls ${DataDir}/*R1*.fastq.gz))
+Num_samp=${#samples[@]}
+window_size=$(( Num_samp / SLURM_ARRAY_TASK_COUNT + 1 ))
+
+lower=$(( SLURM_ARRAY_TASK_ID * window_size ))
+next=$(( SLURM_ARRAY_TASK_ID + 1 ))
+upper=$(( next * window_size ))
+
+if [ "$SLURM_ARRAY_TASK_ID" -eq "$SLURM_ARRAY_TASK_MAX" ]; then
+    upper=$Num_samp
+fi
+
+char="/"
+index=$(echo "${DataDir}" | awk -F"${char}" '{print NF-1}')
+index=$(( index + 2 ))
+
+for((i=$lower;i<$upper;i++))
+do
+
+    name=${samples[$i]}
+    R1=$name
+    R2=${name/R1/R2}
+    
+    sample_name=$(echo $name| cut -d'/' -f $index)
+    sample_name=$(echo ${sample_name%R1*.fastq.gz})
+    temp_dir=${OutDir}/temp/${sample_name}
+	OutDir=${OutDir}/${sample_name}
+
+    echo start sample $sample_name
+    
+    STAR --runThreadN 16 \
+         --quantMode GeneCounts \
+         --genomeDir $GenomeIndexDir \
+         --outSAMtype None \
+         --outFileNamePrefix ${OutDir} \
+         --readFilesIn $R1 $R2 \
+         --readFilesCommand zcat \
+         --alignSJoverhangMin 8 \
+         --alignSJDBoverhangMin 1 \
+         --outFilterMultimapNmax 20 \
+         --genomeLoad LoadAndKeep \
+         --limitBAMsortRAM 50000000000 \
+         --outFilterScoreMinOverLread 0.5 \
+         --outFilterMatchNminOverLread 0.5 \
+         --outTmpDir $temp_dir \
+
+done
+
+echo Job finished on:
+date -u
