65472e439c41ff7febc2d9df7d74dc19379231c2
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-02-24 23:45:38 +0000

    Update Genome-Indexing.sh

--- Mapping-to-Ref-Genome/Salmon/Genome-Indexing.sh
+++ Mapping-to-Ref-Genome/Salmon/Genome-Indexing.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+#SBATCH -A Research_Project-MRC164847 # research project to submit under.
+#SBATCH --export=ALL # export all environment variables to the batch job.
+#SBATCH -D . # set working directory to .
+#SBATCH -p mrcq # submit to the parallel test queue
+#SBATCH --time=5:00:00 # Maximum wall time for the job
+#SBATCH --nodes=1 # specify number of nodes.
+#SBATCH --ntasks-per-node=16 # specify number of processors.
+#SBATCH --mail-type=END # send email at job completion
+#SBATCH --mail-user=m.kouhsar@exeter.ac.uk # email address
 
+Genome_fasta=./Genome/genome_transcripts.fa.gz
+OutDir=./Salmon/Genome_Index
+Decoy_file=./Salmon/decoy.txt
+
+module load Salmon
+
+salmon index -t $Genome_fasta  \
+	-i $OutDir \
+	--decoys $Decoy_file \
+  -p 16 \
+  --gencode
+
+echo "Indexing process has been done!"
