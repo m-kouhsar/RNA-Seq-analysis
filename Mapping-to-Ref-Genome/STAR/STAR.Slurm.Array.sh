9e4e4066e5638a19039c8ebcd2cd8e06aba3af8d
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-11-10 23:39:16 +0000

    Update STAR.Slurm.Array.sh

--- Mapping-to-Ref-Genome/STAR/STAR.Slurm.Array.sh
+++ Mapping-to-Ref-Genome/STAR/STAR.Slurm.Array.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH -A Research_Project1 # research project to submit under.
-#SBATCH --export=ALL # export all environment variables to the batch job.
-#SBATCH -D . # set working directory to .
-#SBATCH -p mrcq # submit to the parallel test queue
-#SBATCH --time=5:00:00 # Maximum wall time for the job
-#SBATCH --nodes=1 # specify number of nodes.
-#SBATCH --ntasks-per-node=16 # specify number of processors.
-#SBATCH --mail-type=END # send email at job completion
-#SBATCH --mail-user=m.kouhsar@exeter.ac.uk # email address
+#SBATCH -A Research_Project1                            # research project to submit under.
+#SBATCH --export=ALL                                    # export all environment variables to the batch job.
+#SBATCH -D .                                            # set working directory to .
+#SBATCH -p mrcq                                         # submit to the parallel test queue
+#SBATCH --time=5:00:00                                  # Maximum wall time for the job
+#SBATCH --nodes=1                                       # specify number of nodes.
+#SBATCH --ntasks-per-node=16                            # specify number of processors.
+#SBATCH --mail-type=END                                 # send email at job completion
+#SBATCH --mail-user=m.kouhsar@exeter.ac.uk              # email address
 #SBATCH --array=0-15
 #SBATCH --job-name=STAR
 
