7df08212c54914a8c7bb3b0489ac08794a0f28bb
Kouhsar <mk693@login02.cluster.local>
2025-07-31 00:10:25 +0100

    Updated

--- Count-Matrix-Analysis/Visualization/Visualization.R
+++ Count-Matrix-Analysis/Visualization/Visualization.R
@@ -3,6 +3,7 @@ message("Loading required libraries...")
 
 suppressMessages(suppressWarnings(library(ggplot2)))
 suppressMessages(suppressWarnings(library(qqman)))
+suppressMessages(suppressWarnings(library(EnhancedVolcano)))
 
 #########################################################
 args = commandArgs(T)
@@ -11,18 +12,33 @@ results_file = args[1]
 gene_col = args[2]
 logFC_col = args[3]
 pvalue_col = args[4]
-pvalue_cut = args[5]
-QQ.title = args[6]
-Volcano.title = args[7]
-OutPrefix = args[8]
+pvalue_cut = as.numeric(trimws(args[5]))
+logFC_cut = as.numeric(trimws(args[6]))
+QQ.title = args[7]
+Volcano.title = args[8]
+OutPrefix = args[9]
+
+cat("##########################################################################\n")
+message("Input arguments:")
+message("        DEG results file: ",results_file)
+message("        Gene IDs column name: ",gene_col)
+message("        Log fold change column name: ",logFC_col)
+message("        P-values column name: ",pvalue_col)
+message("        P-value threshold: ",pvalue_cut)
+message("        logFC threshold: ",logFC_cut)
+message("        QQ plot title: ",QQ.title)
+message("        Volcano plot title:",Volcano.title)
+message("        Output files prefix: ",OutPrefix)
+cat("##########################################################################\n")
 
 ########################################################
-
+message("Reading the data...")
 results.table <- read.csv(results_file , stringsAsFactors = F)
 
 chisq <- qchisq(1-results.table[,pvalue_col],1)
 inflation <- median(chisq, na.rm = T)/qchisq(0.5,1)
 
+message("Generating plots...")
 tiff(filename = paste0(OutPrefix , ".QQ.tif") , res = 300 , units = "in" , height = 8 , width = 8)
 qq(results.table[,pvalue_col], main=QQ.title)
 text(x = 0.5,y = (par("usr")[4]-0.2),
@@ -33,6 +49,8 @@ graphics.off()
 
 tiff(filename = paste0(OutPrefix , ".volcano.tif") , res = 300 , units = "in" , height = 8 , width = 8)
 
-EnhancedVolcano(toptable = results.table,lab = gene_col , x = logFC_col , y = pvalue_col , pCutoff = pvalue_cut,title = Volcano.title)
+EnhancedVolcano(toptable = results.table,lab = results.table[,gene_col] , x = logFC_col , y = pvalue_col , 
+                pCutoff = pvalue_cut,title = Volcano.title,FCcutoff = logFC_cut,
+                legendPosition = "right",drawConnectors=T)
 
 graphics.off()
--- Count-Matrix-Analysis/Visualization/Visualization.sh
+++ Count-Matrix-Analysis/Visualization/Visualization.sh
@@ -11,13 +11,19 @@
 #SBATCH --output=%x.%j.out
 #########################################################################
 
-results_file=
-gene_col=args[2]
-logFC_col=args[3]
-pvalue_col=args[4]
-pvalue_cut=args[5]
-QQ_title=args[6]
-Volcano_title=args[7]
-OutPrefix=args[8]
+results_file="/lustre/projects/Research_Project-T111004/Morteza/Ascribed_Study/Results/Ascribed.DESeq2.Confusio.Non_Conf.csv"
+gene_col="X"
+logFC_col="logFC"
+pvalue_col="PValue"
+pvalue_cut="0.01"
+QQ_title="QQ Plot"
+Volcano_title="Volcano Plot"
+OutPrefix="Results/Ascribed.DESeq2.Confusio.Non_Conf"
 
 ScriptDir=/lustre/projects/Research_Project-T111004/Morteza/github/RNA_Seq_analysis/Count-Matrix-Analysis/Visualization
+
+###########################################################################
+
+Rscript $ScriptDir/Visualization.R "$results_file" "$gene_col" "$logFC_col" "$pvalue_col" "$pvalue_cut" "$QQ_title" "$Volcano_title" "$OutPrefix"
+
+echo "All done!"
