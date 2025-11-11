5554bf1b081609d5e367cee615e18429c19c996c
Kouhsar <mk693@login02.cluster.local>
2025-11-11 14:21:57 +0000

    Updated

--- Count-Matrix-Analysis/3.Results.Visualization.sh
+++ Count-Matrix-Analysis/3.Results.Visualization.sh
@@ -11,19 +11,20 @@
 #SBATCH --output=%x.%j.out
 #########################################################################
 
-results_file="/lustre/projects/Research_Project-T111004/Morteza/Ascribed_Study/Results/Ascribed.DESeq2.Confusio.Non_Conf.csv"
-gene_col="X"
+results_file="./Results/NIH.limma.AD.C.csv"
+gene_col="Gene"
 logFC_col="logFC"
 pvalue_col="PValue"
 pvalue_cut="0.01"
+logFC_cut="1"
 QQ_title="QQ Plot"
 Volcano_title="Volcano Plot"
-OutPrefix="Results/Ascribed.DESeq2.Confusio.Non_Conf"
+OutPrefix="./Results/NIH.limma"
 
-ScriptDir=/lustre/projects/Research_Project-T111004/Morteza/github/RNA_Seq_analysis/Count-Matrix-Analysis/Visualization
+ScriptDir="./RNA-Seq-analysis/Count-Matrix-Analysis"
 
 ###########################################################################
 
-Rscript $ScriptDir/Visualization.R "$results_file" "$gene_col" "$logFC_col" "$pvalue_col" "$pvalue_cut" "$QQ_title" "$Volcano_title" "$OutPrefix"
+Rscript $ScriptDir/Visualization.R "$results_file" "$gene_col" "$logFC_col" "$pvalue_col" "$pvalue_cut" "$logFC_cut" "$QQ_title" "$Volcano_title" "$OutPrefix"
 
 echo "All done!"
